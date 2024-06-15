183313cfdfd739853e73abf231354f82b65d58fc
lucas <lucas.chorolqui@gmail.com>
2024-06-15 13:19:03 -0300

    setup - dependencies

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,21 @@
-import prisma from "@/lib/db";
+import { Separator } from "@/components/ui/separator";
 
 export default async function Home() {
-  const tragos = await prisma.drink.findMany();
-
   return (
-    <div>
-      {tragos.map((trago) => (
-        <div key={trago.id}>{trago.name}</div>
-      ))}
-    </div>
+    <section>
+      <picture className="w-12 h-12">
+        <img src="/logo.png" alt="Logo" className="w-full h-full" />
+      </picture>
+
+      <div className="w-full h-full flex justify-start">
+        <title className="text-2xl text-white font-semibold">
+          Administrador
+        </title>
+      </div>
+
+      <Separator />
+
+      <form>formulario</form>
+    </section>
   );
 }
